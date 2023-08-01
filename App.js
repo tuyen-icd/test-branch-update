7bf2bf98de14418883e876f307d55811138b76a2
tuyennn_icd <tuyennn@icd-vn.com>
2023-08-01 16:27:09 +0700

    test branch update testt testt

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Viewtest from './Viewtest';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST EAS UPDATE</Text>
+      <Text>TESTTTT</Text>
+      <Viewtest />
       <StatusBar style="auto" />
     </View>
   );
